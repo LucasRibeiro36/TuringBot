305bc5677842cd8500ae457add4b8d7f99a7cd57
LucasRibeiro36 <lucasribeiro5569@gmail.com>
2023-07-06 04:23:15 -0300

    configurando dotenv

--- config/database.js
+++ config/database.js
@@ -1,13 +1,15 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 //mysql
-const sequelize = new Sequelize('Bot', 'root', 'password', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
-    logging: false,
     define: {
-        timestamps: false,
-    },
+        timestamps: false
+    }
 });
 
 
